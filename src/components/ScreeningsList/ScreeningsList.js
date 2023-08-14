cc1c13b1f5703a261bcf5999386e416d00029a21
Isaac Jiang <isaac.jiang66@gmail.com>
2023-08-14 10:58:14 -0400

    new screenings list

--- src/components/ScreeningsList/ScreeningsList.js
+++ src/components/ScreeningsList/ScreeningsList.js
@@ -3,25 +3,25 @@ import './ScreeningsList.css';
 
 // Sample screening data in the format: "Screening Name\nDescription\nFrequency"
 const screeningData = `
-1. Screening: Lipid profile (cholesterol levels)
-Frequency: Every 5 years.
-Description: This screening checks your cholesterol levels to assess your risk for heart disease and helps determine if lifestyle changes or medication are necessary.
+1. Screening: Comprehensive Eye Exam
+Frequency: every 1-2 years
+Description: A comprehensive eye exam evaluates your vision and checks for any changes or abnormalities that may require further attention.
 
-2. Screening: Colonoscopy or colorectal screening
-Frequency: Starting at age 50, every 10 years.
-Description: This screening helps detect and prevent colorectal cancer by examining the colon and rectum for abnormalities or early signs of cancer.
+2. Screening: Blood Pressure Measurement
+Frequency: every 1-2 years, or more frequently if indicated
+Description: Regular blood pressure measurements help monitor your cardiovascular health, detect high blood pressure, and assess your risk for heart disease.
 
-3. Screening: Bone density test (DEXA scan)
-Frequency: Starting at age 50 for women, every 2 years.
-Description: This test evaluates your bone health and helps identify osteoporosis or low bone density, which can increase the risk of fractures.
+3. Screening: Colorectal Cancer Screening (Colonoscopy)
+Frequency: every 10 years, starting at age 50 or earlier if recommended by your healthcare provider
+Description: A colonoscopy helps detect early signs of colorectal cancer and other abnormal conditions in the colon and rectum.
 
-4. Screening: Mammogram
-Frequency: Starting at age 50, every 2 years.
-Description: A mammogram is used to detect breast cancer in its early stages, allowing for more effective treatment options and improved outcomes.
+4. Screening: Blood Glucose Test
+Frequency: every 3 years, starting at age 45 or earlier if you have additional risk factors
+Description: A blood glucose test checks for diabetes or prediabetes, evaluating your blood sugar levels and assessing your risk for developing diabetes.
 
-5. Screening: Diabetes screening (blood sugar levels)
-Frequency: Starting at age 45, every 3 years.
-Description: This test assesses your blood sugar levels to screen for diabetes and identify risk factors for developing the condition.
+5. Screening: Fasting Lipid Profile
+Frequency: every 4-6 years, or more frequently if indicated
+Description: A fasting lipid profile measures your cholesterol and triglyceride levels, aiding in the assessment of your cardiovascular health and risk for heart disease.
 `;
 
 function parseScreeningData(data) {
