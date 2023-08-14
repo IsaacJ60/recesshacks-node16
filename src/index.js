f40a845688863b9facc45119447626114324331a
Isaac Jiang <isaac.jiang66@gmail.com>
2023-08-14 09:17:23 -0400

    favicons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+    <link rel="icon" type="image/png" href="recesshacks2\favicon_package_v0.16\favicon-32x32.png" />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
